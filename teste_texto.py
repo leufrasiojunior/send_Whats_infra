9fdbe4d0e6f548d470a77fa3a006e9dac61ca610
leonald <leufrasiojunior@gmail.com>
2023-08-13 22:46:00 -0300

    Log criado.

--- teste_texto.py
+++
@@ -1,7 +0,0 @@
-include_name = False  # Set this to True or False based on user input
-
-age = 30
-name = "Alice" if include_name else ""
-
-formatted_string = f"My name is {name} and I am {age} years old."
-print(formatted_string)
