baf333876f17b4d2dd424c65f854886178e93cec
Leonald <leufrasiojunior@gmail.com>
2023-11-15 22:05:04 -0300

    adjust

--- teste.py
+++ teste.py
@@ -1,29 +1,15 @@
-import logging
+import requests
 
-def configure_logger(file_name, level=logging.INFO):
-    # Set the desired log level
-    logging.basicConfig(level=level)
+url = "https://whatsapp-number-validator3.p.rapidapi.com/WhatsappNumberHasIt"
 
-    # Create a logger with the file name
-    logger = logging.getLogger(file_name)
+payload = { "number": "551197979797" }
+headers = {
+	"content-type": "application/json",
+	"X-RapidAPI-Key": "43540365d7msh166745d0c9aa746p112efcjsn534dec584af4",
+	"X-RapidAPI-Host": "whatsapp-number-validator3.p.rapidapi.com"
+}
 
-    # Create a handler to save logs to a file
-    file_handler = logging.FileHandler("logs/" + file_name + '.log')
+response = requests.post(url, json=payload, headers=headers)
+statusNumber = response.json()
 
-    # Define the log format
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(funcName)s')
-    file_handler.setFormatter(formatter)
-
-    # Add the handler to the logger
-    logger.addHandler(file_handler)
-
-    return logger
-
-# Example usage of the function
-
-my_logger = configure_logger('log', level=logging.DEBUG)
-my_logger.debug('This is a debug message')
-my_logger.info('This is an information message')
-my_logger.warning('This is a warning message')
-my_logger.error('This is an error message')
-my_logger.critical('This is a critical message')
+print(statusNumber["status"])
